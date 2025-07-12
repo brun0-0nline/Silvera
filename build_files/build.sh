e5bcc2e58bc95a2af3f2b7c3127f2769ea84c290
brun0-0nline <brun0.0nline@pm.me>
2025-07-12 19:01:23 -0300

    Setup base image

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,37 @@
 
 set -ouex pipefail
 
-### Install packages
+# Install dnf5 if not installed
+if ! rpm -q dnf5 >/dev/null; then
+    rpm-ostree install dnf5
+fi
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+### Remove packages
+dnf5 -y remove firefox \
+               firefox-langpacks
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+### Post removal cleanup
+dnf5 clean all
+
+### Install DNF5 plugins to get the config-manager
+dnf5 -y in dnf5-plugins
+
+# Add and verify rpmfusion repos
+dnf5 -y in distribution-gpg-keys && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-fedora-$(rpm -E %fedora) && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-nonfree-fedora-$(rpm -E %fedora) && \
+dnf5 -y --setopt=localpkg_gpgcheck=1 in https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+										https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+### Install packages for codecs and hardware acceleration
+dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
+dnf5 -y in libva-intel-driver
+
+# Install distrobox and packages required by homebrew
+dnf5 -y in distrobox @development-tools \
+                     procps-ng \
+                     curl \
+                     file
 
 # Use a COPR Example:
 #
@@ -19,6 +41,6 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+#### Enabling a System Unit File
 
 systemctl enable podman.socket
