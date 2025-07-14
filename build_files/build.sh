f77a8d1b1c7bdaaddedcc3a7522c34f2517896c8
brun0-0nline <brun0.0nline@pm.me>
2025-07-14 01:23:26 -0300

    Extra image updates

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,15 +7,6 @@ if ! rpm -q dnf5 >/dev/null; then
     rpm-ostree install dnf5
 fi
 
-### Remove packages
-dnf5 -y remove firefox \
-               firefox-langpacks \
-               gnome-extensions-app \
-               yelp
-
-### Post removal cleanup
-dnf5 clean all
-
 ### Install DNF5 plugins to get the config-manager
 dnf5 -y in dnf5-plugins
 
@@ -30,12 +21,35 @@ dnf5 -y --setopt=localpkg_gpgcheck=1 in https://mirrors.rpmfusion.org/free/fedor
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing
 dnf5 -y in libva-intel-driver
 
-# Install distrobox, gnome tweaks and packages required by homebrew
-dnf5 -y in distrobox gnome-tweaks \
-                     @development-tools \
-                     procps-ng \
-                     curl \
-                     file
+# Install packages required by homebrew
+dnf5 -y in  @development-tools \
+            procps-ng \
+            curl \
+            file
+
+### Install packages i want to use
+dnf5 -y in  distrobox \
+            gnome-tweaks \
+            chromium \
+            chromedriver \
+            ptyxis
+
+### Remove packages
+dnf5 -y remove  fedora-bookmarks \
+                fedora-chromium-config \
+                fedora-chromium-config-gnome \
+                fedora-logos \
+                firefox \
+                firefox-langpacks \
+                gnome-extensions-app \
+                gnome-shell-extension-background-logo \
+                gnome-software-rpm-ostree \
+                gnome-console \
+                yelp
+
+### Post removal cleanup
+dnf5 clean all
+
 
 #### Enabling a System Unit File
 # Left this here cause it's on the template
